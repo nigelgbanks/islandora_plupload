3d3714cddc19506af4ef9aed73d184604d038c08
Nigel Banks <nigel.g.banks@gmail.com>
2014-12-12 11:41:49 -0500

    This change is for when a user submits the form without first uploading the files.
    In particular when using more than one plupload element in the form, although
    it did not work with just a single plupload previously.

--- js/element.js
+++ js/element.js
@@ -1,43 +1,61 @@
+/*jshint browser: true, devel:true*/
+/*global jQuery, Drupal, plupload*/
 /**
  * @file
  * Enforce max_file_count for Plupload elements.
  */
 (function($) {
+  'use strict';
   Drupal.behaviors.islandoraPlupload = {
     attach: function (context, settings) {
-      $('form').find('.plupload-element').each( function(index){
-        var element = $(this);
-        var uploader = $(this).pluploadQueue();
-        var id = $(this).attr('id');
-        var maxFiles = settings.plupload[id].max_file_count;
+      $('form', context).find('.plupload-element').each(function (index) {
+        $(this).once('islandoraPlupload', function () {
+          var $element = $(this);
+          var uploader = $element.pluploadQueue();
+          var id = $element.attr('id');
+          var maxFiles = settings.plupload[id].max_file_count;
 
-        if (id && maxFiles) {
-          uploader.bind('FilesAdded', function(up, files) {
-            var maxFiles = settings.plupload[id].max_file_count;
-            if (up.files.length > maxFiles) {
-              up.splice(maxFiles);
-              alert(Drupal.formatPlural(maxFiles, 'Only one file may be uploaded.', 'Only @count files may be uploaded.'));
-            }
-            if (up.files.length === maxFiles) {
-              $('.plupload_add', element).hide('slow');
-            }
-          });
-          uploader.bind('FilesRemoved', function(up, files) {
-            if (up.files.length < maxFiles) {
-              $('.plupload_add', element).show('slow');
-            }
-          });
-          uploader.bind('FileUploaded', function(up, file, res) {
-            if (uploader.total.uploaded == uploader.files.length) {
-              if ($($element>'form[class="plupload-form-processed"]').size() > 0) {
-                setTimeout(function() {
-                  $('form[class="plupload-form-processed"]').first().submit();
+          if (id && maxFiles) {
+            uploader.bind('FilesAdded', function (up, files) {
+              var maxFiles = settings.plupload[id].max_file_count;
+              if (up.files.length > maxFiles) {
+                up.splice(maxFiles);
+                alert(Drupal.formatPlural(maxFiles, 'Only one file may be uploaded.', 'Only @count files may be uploaded.'));
+              }
+              if (up.files.length === maxFiles) {
+                $('.plupload_add', $element).hide('slow');
+              }
+            });
+            uploader.bind('FilesRemoved', function (up, files) {
+              if (up.files.length < maxFiles) {
+                $('.plupload_add', $element).show('slow');
+              }
+            });
+            uploader.bind('FileUploaded', function (up, file, res) {
+              // Total is all the pluploader elements in this plupload element's
+              // form.
+              var $form = $element.parents('form');
+              var $pluploaders = $('.plupload-element', $form);
+              var total = $pluploaders.length;
+              var completed = 0;
+              // Only check all if this one is finished.
+              if (uploader.total.uploaded === uploader.files.length) {
+                $pluploaders.each(function (index) {
+                  var uploader = $(this).pluploadQueue();
+                  if (uploader.total.uploaded === uploader.files.length) {
+                    completed++;
+                  }
+                });
+              }
+              if (completed === total) {
+                setTimeout(function () {
+                  $element.parents('form').first().submit();
                 }, 550);
               }
-            }
-          });
-        }
+            });
+          }
+        });
       });
     }
-  }
+  };
 })(jQuery);
