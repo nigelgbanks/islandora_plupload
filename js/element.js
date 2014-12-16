eab3b97c676fd4bbbcdc4945bd90ed32c80ec9f5
Nigel Banks <nigel.g.banks@gmail.com>
2014-12-16 16:21:54 -0500

    Turns out the problem and the work around solution stemmed from the Spinner in Islandora.
    It prevents pluploads default behaviour of clicking on the submit button to
    trigger a submission after the uploads have finished.
    
    So if we no longer provide a default submit button the plupload module will
    default to triggering the submission via the form itself.

--- js/element.js
+++ js/element.js
@@ -10,9 +10,9 @@
     attach: function (context, settings) {
       $('form', context).find('.plupload-element').each(function (index) {
         $(this).once('islandoraPlupload', function () {
-          var $element = $(this);
-          var uploader = $element.pluploadQueue();
-          var id = $element.attr('id');
+          var element = $(this);
+          var uploader = element.pluploadQueue();
+          var id = element.attr('id');
           var maxFiles = settings.plupload[id].max_file_count;
 
           if (id && maxFiles) {
@@ -23,34 +23,12 @@
                 alert(Drupal.formatPlural(maxFiles, 'Only one file may be uploaded.', 'Only @count files may be uploaded.'));
               }
               if (up.files.length === maxFiles) {
-                $('.plupload_add', $element).hide('slow');
+                $('.plupload_add', element).hide('slow');
               }
             });
             uploader.bind('FilesRemoved', function (up, files) {
               if (up.files.length < maxFiles) {
-                $('.plupload_add', $element).show('slow');
-              }
-            });
-            uploader.bind('FileUploaded', function (up, file, res) {
-              // Total is all the pluploader elements in this plupload element's
-              // form.
-              var $form = $element.parents('form');
-              var $pluploaders = $('.plupload-element', $form);
-              var total = $pluploaders.length;
-              var completed = 0;
-              // Only check all if this one is finished.
-              if (uploader.total.uploaded === uploader.files.length) {
-                $pluploaders.each(function (index) {
-                  var uploader = $(this).pluploadQueue();
-                  if (uploader.total.uploaded === uploader.files.length) {
-                    completed++;
-                  }
-                });
-              }
-              if (completed === total) {
-                setTimeout(function () {
-                  $element.parents('form').first().submit();
-                }, 550);
+                $('.plupload_add', element).show('slow');
               }
             });
           }
